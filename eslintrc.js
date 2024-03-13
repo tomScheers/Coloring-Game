58c2a32ffcf06b4ef4b20a1f633c754e4a4efaab
Tom Scheers <150141216+tomScheers@users.noreply.github.com>
2024-03-13 18:59:43 +0100

    deleted some files and for some reason all files got re-added.

---
+++ eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    // Your custom rules here
+  },
+};
